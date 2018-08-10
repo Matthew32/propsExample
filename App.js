7eb3534fe003f65ba843c54e1745d34cc06aaa47
Matthew <mmartinezmateo93@gmail.com>
2018-08-10 12:56:39 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { AppRegistry, View,Image } from 'react-native';
+import Greeting from './app/components/Greeting';
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
+
+/*export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+*/
+
+// skip this line if using Create React Native App
+//AppRegistry.registerComponent('AwesomeProject', () => Bananas);
